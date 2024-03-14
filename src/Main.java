ca6cc78c54edde9e0ebfa56e3750afa705eebff0
Phạm Văn Linh <inyourarea2802@gmail.com>
2024-03-14 08:21:52 +0700

    Answer

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
 
+            String text;
+            while((text = reader.readLine()) != null) {
+                writer.write(text.replaceAll("Nha Trang", "Vũng Tàu") + "\n");
+            }
 
-        Bài làm viết tại đây
+            reader.close();
+            writer.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
     }
 }
